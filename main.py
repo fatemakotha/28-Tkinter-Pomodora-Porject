9b98e749b3798ebdf3831f251436a866b7227b5c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-22 14:40:14 +0600

    Added start and stop button

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "CF0A0A"
-GREEN = "#9bdeac"
+GREEN = "#2B7A0B"
 YELLOW = "#f7f5dd"
 BLACK = "black"
-ORANGE = "DC5F00"
+ORANGE = "#DC5F00"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -24,23 +24,30 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=BLACK) #bg changes background color
 
-#Putting an image in the background:
+
 #Create a canvas using the canvas widget:
 canvas = Canvas(width=200, height=224, bg=BLACK, highlightthickness=0) #bg changes background color #highlightthickness=0 removes the white border of canvas
-
+#Putting an image in the background:
 #Add image to canvas:
 tomato_img = PhotoImage(file="tomato - Copy (2).png") #PhotoImage reads through a file and gets hold of an image
 canvas.create_image(100, 112, image=tomato_img) #inserts image to the background at the x=12 and y=112
-
 #Create text in canvas:
 canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")) #inserts text to the background at the x=12 and y=130
-canvas.pack() #calls the canvas function to display
+canvas.grid(column=1, row=1) #calls the canvas function to display
+
+#Creating the "Timer" Label:
+title_label = Label(text="Timer", fg=GREEN, bg=BLACK, font=(FONT_NAME, 30, "bold"))
+title_label.grid(column=1, row=0)
 
 
+#Creating the START Button:
+start_button = Button(text="Start", width=8, bg=ORANGE, fg="white", font=(FONT_NAME, 15, "bold"))
+start_button.grid(column=0, row=2)
 
 
-
-
+#Creating the STOP Button:
+stop_button = Button(text="Stop", width=8, bg=ORANGE, fg="white", font=(FONT_NAME, 15, "bold"))
+stop_button.grid(column=2, row=2)
 
 
 
