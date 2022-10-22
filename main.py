5dcea93602f939def0ea1bb776aaaf5cd55f5cda
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-22 14:03:27 +0600

    Added text to canvas

--- main.py
+++ main.py
@@ -25,9 +25,13 @@ window.config(padx=100, pady=50)
 #Putting an image in the background:
 #Create a canvas using the canvas widget:
 canvas = Canvas(width=200, height=224)
+
 #Add image to canvas:
 tomato_img = PhotoImage(file="tomato - Copy (2).png") #PhotoImage reads through a file and gets hold of an image
 canvas.create_image(102, 112, image=tomato_img) #inserts image to the background
+
+#Create text in canvas:
+canvas.create_text(102, 112, text="00:00")
 canvas.pack() #calls the canvas function to display
 
 
