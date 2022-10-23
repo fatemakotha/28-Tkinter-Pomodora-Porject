96649704d16acf4615dbd22af848a9c045e5e788
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-23 16:10:39 +0600

    Timer up and running

--- main.py
+++ main.py
@@ -8,21 +8,35 @@ YELLOW = "#f7f5dd"
 BLACK = "black"
 ORANGE = "#DC5F00"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     """#Let's say we start from 245 mins. Then 245 / 60 = 4.083 MINS, which means 4MINS and 0.083SECS
        #Now if we do 245 % 60 = 5
-       #Now evertime the loops runs, the count_min and count_secs DECREASES by 1"""
+       #Now every time the loops runs, the count_min and count_secs DECREASES by 1"""
 
     count_min = math.floor(count / 60) #math.floor() returns the largest whole number that is less <= x. IN THIS CASE ITS 4 **
     count_seconds = count % 60 #gives the remainder number of seconds WHICH IS 5 **
@@ -36,11 +50,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_seconds}") #timer_text is the name of the item to be configured, and text is what it is to be configured into
     if count > 0:
         window.after(1000, count_down, count - 1) #starts after 1000ms and starts from 5, as count_down(5) is given as input. And decreases by 1
-
-
-
-
-
+    elif count == 0:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -48,9 +59,6 @@ def count_down(count):
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=BLACK) #bg changes background color
-
-
-
 
 
 #Create a canvas using the canvas widget:
@@ -64,7 +72,6 @@ timer_text = canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT
 canvas.grid(column=1, row=1) #calls the canvas function to display
 
 
-
 #Creating the "Timer" Label:
 title_label = Label(text="Timer", fg=GREEN, bg=BLACK, font=(FONT_NAME, 50, "bold"))
 title_label.grid(column=1, row=0)
@@ -75,10 +82,10 @@ start_button = Button(text="Start", width=8, bg=ORANGE, fg="white", font=(FONT_N
 start_button.grid(column=0, row=2)
 
 
-
 #Creating the STOP Button:
 reset_button = Button(text="Stop", width=8, bg=ORANGE, fg="white", font=(FONT_NAME, 15, "bold"))
 reset_button.grid(column=2, row=2)
+
 
 #Check mark Label:
 check_marks = Label(text="✅", fg=GREEN, bg=BLACK, font=(FONT_NAME, 15, "bold"))
